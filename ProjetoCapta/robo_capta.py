f3efdf33936df8933109c20d0c3fdf4615dd7c9a
DouglasAmaralPV <douglas.amaral2006@gmail.com>
2026-03-27 00:22:08 -0300

    amanheeucontinuo

--- ProjetoCapta/robo_capta.py
+++ ProjetoCapta/robo_capta.py
@@ -4,7 +4,12 @@ from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 import time
 
-print("🚀 Iniciando o Robôzinho da Roteirização CaptaVT ")
+#Leitor da lista de CPFS
+
+with open('cpfs_reais.txt', 'r') as arquivo:
+	cpfs = arquivo.readlines()
+    
+print(f"🚀 Iniciando o Robôzinho da Roteirização {len(cpfs)} CaptaVT ")
 
 # Configuração do Selenium para abrir o navegador (Chrome)
 servico = Service(ChromeDriverManager().install())
@@ -37,56 +42,75 @@ print("✅ Login realizado com sucesso! Acessando a Capta...")
 # Pausa de 10 segundos para o processo de login ser concluído
 time.sleep(10)
 
-#Acessar a roteirização
+#Loop e contador
+contador = 0
+for cpf in cpfs:
+    cpf_reais = cpf.strip() 
+    if not cpf_reais:
+        continue 
+    
+    contador += 1 
+    print(f"[{contador}/145] Processando CPF: {cpf_reais}")
+    
+    try:
+        #Acessa a página de consulta
 
-navegador.find_element(By.XPATH, '//*[@id="root"]/div[1]/div/div/aside/nav/div[3]/div/h2/button/div').click()
-time.sleep(2)
+        
 
-#Clicando em Pesquisar Consulta
+        #Acessa a roteirização
+        navegador.find_element(By.XPATH, '//*[@id="root"]/div[1]/div/div/aside/nav/div[3]/div/h2/button/div').click()
+        time.sleep(5)
 
-navegador.find_element(By.XPATH, '//*[@id="root"]/div[1]/div/div/aside/nav/div[3]/div/div/div/a[3]/div').click()
-time.sleep(2)
+        #Clicando em Pesquisar Consulta
 
-#Clicando em "POR CPF"
+        navegador.find_element(By.XPATH, '//*[@id="root"]/div[1]/div/div/aside/nav/div[3]/div/div/div/a[3]/div').click()
+        time.sleep(2)
 
-navegador.find_element(By.XPATH, '//*[@id="root"]/div[1]/div/div/div[3]/button[1]').click()
-time.sleep(2)
+        #Clicando em "POR CPF"
 
-navegador.find_element(By.XPATH,'/html/body/div[4]/div/div/div/div[2]/div/div/input').click()
-time.sleep(2)
+        navegador.find_element(By.XPATH, '//*[@id="root"]/div[1]/div/div/div[3]/button[1]').click()
+        time.sleep(2)
 
-#Insere o CPF
-navegador.find_element(By.XPATH,'/html/body/div[4]/div/div/div/div[2]/div/div/input').send_keys("50180680862")
-time.sleep(2)
+        navegador.find_element(By.XPATH,'/html/body/div[4]/div/div/div/div[2]/div/div/input').click()
+        time.sleep(2)
 
-#Clica em "Pesquisar"
-navegador.find_element(By.XPATH, '/html/body/div[4]/div/div/div/div[3]/button[2]').click()
-time.sleep(5)
+        #Insere o CPF
+        navegador.find_element(By.XPATH,'/html/body/div[4]/div/div/div/div[2]/div/div/input').send_keys(cpf_reais)
+        time.sleep(2)
 
-#CLicar no OLHO e abrir a rota
+        #Clica em "Pesquisar"
+        navegador.find_element(By.XPATH, '/html/body/div[4]/div/div/div/div[3]/button[2]').click()
+        time.sleep(5)
 
-navegador.execute_script("window.scrollBy(0, 500);") # Scroll para baixo para encontrar o "Olho"
-time.sleep(2)
+        #CLicar no OLHO e abrir a rota
 
-navegador.find_element(By.XPATH, '//*[@id="root"]/div[1]/div/div/div[3]/div[6]/div/div/div/div/div/div[2]/a/img').click()
-time.sleep(5)
+        navegador.execute_script("window.scrollBy(0, 500);") # Scroll para baixo para encontrar o "Olho"
+        time.sleep(2)
 
-#Roteirizando e Enviando o E-MAIL
-navegador.execute_script("window.scrollBy(0, 300);") # Scroll para baixo para encontrar o "Olho"
-time.sleep(2)
+        navegador.find_element(By.XPATH, '//*[@id="root"]/div[1]/div/div/div[3]/div[6]/div/div/div/div/div/div[2]/a/img').click()
+        time.sleep(5)
 
-navegador.find_element(By.XPATH, '//*[@id="root"]/div[1]/div/div/div[2]/header[2]/div[2]/button[1]').click()
-time.sleep(55)
-time.sleep(2)
-print("✅ Roteirização realizada, próximo passo, enviar o e-mail")
+        #Roteirizando e Enviando o E-MAIL
+        navegador.execute_script("window.scrollBy(0, 300);") # Scroll para baixo para encontrar o "Olho"
+        time.sleep(2)
 
-#Clicar no MyLink e envia o e-mail
+        navegador.find_element(By.XPATH, '//*[@id="root"]/div[1]/div/div/div[2]/header[2]/div[2]/button[1]').click()
+        time.sleep(55)
+        time.sleep(2)
+        print("✅ Roteirização realizada, próximo passo, enviar o e-mail")
 
-navegador.find_element(By.XPATH, '//*[@id="root"]/div[1]/div/div/div[2]/header[2]/div[2]/button[5]').click()
-time.sleep(3) # clica no MyLink
+        #Clicar no MyLink e envia o e-mail
 
-navegador.find_element(By.XPATH, '/html/body/div[4]/div/div/div/div[3]/button[2]').click()
-time.sleep(3) # clica em "Enviar Link"
+        navegador.find_element(By.XPATH, '//*[@id="root"]/div[1]/div/div/div[2]/header[2]/div[2]/button[5]').click()
+        time.sleep(3) # clica no MyLink
 
-print("✅ E-mail enviado com sucesso! Processo concluído.")
+        navegador.find_element(By.XPATH, '/html/body/div[4]/div/div/div/div[3]/button[2]').click()
+        time.sleep(3) # clica em "Enviar Link"
 
+        print(f"   ✅ E-mail do CPF {cpf_reais} enviado com sucesso! Processo concluído.")
+
+    except Exception as e:
+        print(f"❌ Ocorreu um erro no CPF {cpf_reais}: {e} - Continuando para o próximo CPF.")
+        continue
+
+    print("\n 🎉Trabalho concluido! Todos os CPF foram roteirizados e enviados") # Linha em branco para melhor visualização entre os CPFs
