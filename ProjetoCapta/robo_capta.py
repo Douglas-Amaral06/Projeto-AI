c76072c256fc3bf1210fe7653ec68533f249d402
DouglasAmaralPV <douglas.amaral2006@gmail.com>
2026-03-26 22:18:06 -0300

    CPFSendoAdicionado

--- ProjetoCapta/robo_capta.py
+++ ProjetoCapta/robo_capta.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 import time
 
@@ -13,12 +14,12 @@ navegador = webdriver.Chrome(service=servico) #lembrar de definir Chrome como pa
 link_login = "https://app.captamobilidade.com.br/" # (Link raiz para o site de login da Capta)
 navegador.get(link_login)
 
-navegador.maxime_window() # F11 no google
+navegador.maximize_window() # F11 no google
 
 # Pausa de 10 segundos para fazer o login manualmente (caso necessário  )
 time.sleep(10)
 
 #Implementar o CPF
 
-navegador.find_element(by.XPATH, '//*[@id="root"]/div[2]/div[2]/div[4]/div[1]/div/div/div/input').send_keys("49742274835")
+navegador.find_element(By.XPATH, '//*[@id="root"]/div[2]/div[2]/div[4]/div[1]/div/div/div/input').send_keys("49742274835")
 time.sleep(2)
